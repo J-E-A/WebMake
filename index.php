a9b5bdb957735e51697c28503a01ed500c2be6d8
AEdrian <edrianjosef@gmail.com>
2018-03-27 22:48:23 +0800

    Big change, changed dir name, added site.manifest, and index.php title,

--- index.php
+++ index.php
@@ -1,10 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="UTF-8">
-	<title>Document</title>
+	<meta charset="utf-8">
+        <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+        <title>WebMake</title>
+
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="manifest" href="site.webmanifest">
+        <link rel="shortcut icon" href="icon.png" type="image/png">
+
 </head>
 <body>
-	
+	<nav class="jea-nav">
+		<ul>
+			<li></li>
+		</ul>
+	</nav>
 </body>
 </html>
