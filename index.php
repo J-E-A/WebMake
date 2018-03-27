7d7937adf795be054148973d85d84cc5380bc458
AEdrian <edrianjosef@gmail.com>
2018-03-27 23:00:03 +0800

    aadded google analytics in the index.php file

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="manifest" href="site.webmanifest">
         <link rel="shortcut icon" href="icon.png" type="image/png">
+        <link rel="stylesheet" href="_lib/normalize/index.css">
         <link rel="stylesheet" href="_sys/css/main.css">
 
 </head>
@@ -22,5 +23,13 @@
 
 	<!--Script Tags-->
 	<script src="_sys/js/main.js"></script>
+
+
+	<!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+        <script>
+            window.ga=function(){ga.q.push(arguments)};ga.q=[];ga.l=+new Date;
+            ga('create','UA-XXXXX-Y','auto');ga('send','pageview')
+        </script>
+        <script src="https://www.google-analytics.com/analytics.js" async defer></script>
 </body>
 </html>
